0dc47a1cb2d4ec253434a8f9707562b07d178ffd
Duy Khoa <duykhoa12t@gmail.com>
2015-10-30 17:52:54 +0800

    Fix wrong path problem in shellscript

--- open_list.sh
+++ open_list.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+SCRIPT_DIR=~/.open_list
 APP_DIR=/Applications
+
 app_list="apps.list"
 chrome_list="chrome_tabs.list"
 
-while read -r line
+cd $SCRIPT_DIR
+
+while read line
 do
-  open $APP_DIR/$line.app
-done < $app_list
+  open "$APP_DIR/$line.app"
+done < $SCRIPT_DIR/$app_list
 
 while read -r line
 do
   open $line
-done < $chrome_list
+done < $SCRIPT_DIR/$chrome_list
